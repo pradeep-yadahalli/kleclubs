5bfebcb4840b7e4685c66e1d87a2995b950f90e7
pradeep <01fe24ica027@kletech.ac.in>
2026-09-12 12:33:56 +0530

    Connect backend to local MongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.get("/", (req, res) => {
     res.send("College Event Management System Backend is Running!");
